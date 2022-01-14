32a7ec64b4b06d6ff53363d36a77857e90259de2
Montonen Weerti <weertim@gmail.com>
2022-01-14 13:42:48 +0200

    1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,36 +10,62 @@ const Statistics = (props) => {
   )
 }
 
+const Reviews = (props) => {
+  console.log(props.valueAll)
+  if ( props.valueAll == 0) {
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+  <Statistics text="good" amount={props.good} />
+  <Statistics text="neutral" amount={props.neutral} />
+  <Statistics text="bad" amount={props.bad} />
+  <Statistics text="all" amount={props.valueAll} />
+  <Statistics text="average" amount={props.average} />
+  <Statistics text="positive" amount={props.positive*100} />
+  </div>
+  )
+
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let valueAll = bad + neutral + good
-  let average = (good-bad)/valueAll
-  let positive = good/valueAll
+  const [all, setAll] = useState(0)
+  let average = (good-bad)/all
+  let positive = good/all
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}>
+      <button onClick={() => {
+        setGood(good+1)
+        setAll(all+1)
+       // Reviews({good : {good}, neutral : {neutral}, bad : {bad}, valueAll : {valueAll}, average : {average} , positive : {positive}})
+      }}>
         good
         </button>
-      <button onClick={() => setNeutral(neutral+1)}>
+      <button onClick={() => {
+        setNeutral(neutral+1)
+        setAll(all+1)
+      }}>
         neutral
       </button>
-      <button onClick={() => setBad(bad+1)}>
+      <button onClick={() => {
+        setBad(bad+1)
+        setAll(all+1)
+      }}>
         bad
       </button>
+
       <h1>statistics</h1>
-      <Statistics text="good" amount={good} />
-      <Statistics text="neutral" amount={neutral} />
-      <Statistics text="bad" amount={bad} />
-      <Statistics text="all" amount={valueAll} />
-      <Statistics text="average" amount={average} />
-      <Statistics text="positive" amount={positive*100} />
-      
+     <Reviews good = {good} neutral = {neutral} bad = {bad} valueAll = {all} average = {average}  positive = {positive} />
 
     </div>
 
