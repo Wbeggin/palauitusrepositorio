f4c8235c583f7618104bb5d5a77d62e2e95ae5f4
Wbeggin <weertim@gmail.com>
2022-03-22 16:40:21 +0200

    2.3

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,8 +1,9 @@
-let total = 0
 const Course = ({ course, parts}) => {
-    console.log({course})   
-    console.log({parts})
-
+    const arr = []
+    parts.forEach(element => {
+        arr.push(element.exercises)
+    });
+    const total = arr.reduce( (s, p) => s + p , 0 )
     return(
         <div>
         <h1>{course.name}</h1>
@@ -13,20 +14,12 @@ const Course = ({ course, parts}) => {
                 </li>
                 )}
         </ul>
-
-        {parts.map(part =>
-            calculateTotal(part.exercises)
-         )}
-
         <b>Total of {total} exercises</b>
 
         </div>
     )
 }
 
-const calculateTotal = (number) => {
-    total += number
-}
 
 
 export default Course
