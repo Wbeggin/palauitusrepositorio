59a0142d97d1b20acb52796c1f574dc1f7cc067b
Wbeggin <weertim@gmail.com>
2023-07-19 18:42:49 +0300

    3.8

--- osa3/index.js
+++ osa3/index.js
@@ -3,7 +3,16 @@ const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 morgan.token('data', (req, res) => JSON.stringify(req.body));
-app.use(morgan('tiny'));
+app.use(morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ');
+  }));
 
 let notes = [
     {
