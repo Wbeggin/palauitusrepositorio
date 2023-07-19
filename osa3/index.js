d9ebc49704f4e30c4188f1b099857f467c02100f
Wbeggin <weertim@gmail.com>
2023-07-19 18:12:59 +0300

    3.4

--- osa3/index.js
+++ osa3/index.js
@@ -59,6 +59,17 @@ let notes = [
     response.json(note)
   })
 
+  app.delete('/api/persons/:id/', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+    if (!note) {
+      response.status(404).end();
+    } else {
+      note.number = ''; 
+      response.status(204).end();
+    }
+  });
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
