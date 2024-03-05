86be65878347a899194e0fbe99039fb3ef995d9d
Wbeggin <weertim@gmail.com>
2024-03-05 11:34:05 +0200

    4.4

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -3,8 +3,28 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+})
+
+describe('totallikes', () => {
+    test(' returns the sum of likes from all blogs', () => {
+        const blogs = [
+            { title: 'Blog 1', author: 'Author 1', likes: 10 },
+            { title: 'Blog 2', author: 'Author 2', likes: 20 },
+            { title: 'Blog 3', author: 'Author 3', likes: 30 }
+        ]
+    
+        const result = listHelper.totalLikes(blogs)
+        assert.strictEqual(result, 60)
+    })
+
+    test(' returns 0 if no blogs', () => {
+        const blogs = []
+    
+        const result = listHelper.totalLikes(blogs)
+        assert.strictEqual(result, 0)
+    })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    return likes
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
