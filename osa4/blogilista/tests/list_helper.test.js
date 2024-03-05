880e9d53116e6c3e738f56d2d24de4d481dd2aa3
Wbeggin <weertim@gmail.com>
2024-03-05 11:38:33 +0200

    4.5

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -28,3 +28,27 @@ describe('totallikes', () => {
         assert.strictEqual(result, 0)
     })
 })
+
+describe('favoriteBlog', () => {
+    test('returns the blog with the most likes', () => {
+        const blogs = [
+            { title: 'Blog 1', author: 'Author 1', likes: 10 },
+            { title: 'Blog 2', author: 'Author 2', likes: 20 },
+            { title: 'Blog 3', author: 'Author 3', likes: 30 }
+        ]
+    
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, { title: 'Blog 3', author: 'Author 3', likes: 30 })
+    })
+
+    test('returns the first blog if all have the same amount of likes', () => {
+        const blogs = [
+            { title: 'Blog 1', author: 'Author 1', likes: 10 },
+            { title: 'Blog 2', author: 'Author 2', likes: 10 },
+            { title: 'Blog 3', author: 'Author 3', likes: 10 }
+        ]
+    
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, { title: 'Blog 1', author: 'Author 1', likes: 10 })
+    })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,8 +6,15 @@ const totalLikes = (blogs) => {
     const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
     return likes
 }
+
+const favoriteBlog = (blogs) => {
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+    const favorite = blogs.find(blog => blog.likes === maxLikes)
+    return favorite
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
