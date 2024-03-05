1bb26039d290233bd5b289650a146d01c8701144
Wbeggin <weertim@gmail.com>
2024-03-05 13:32:51 +0200

    4.10

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,14 +2,14 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false)
 
-app.use(cors())
-app.use(express.json())
 
 const PORT = config.PORT
 const mongoUrl = config.MONGODB_URI
--- osa4/blogilista/tests/blogs.test.js
+++ osa4/blogilista/tests/blogs.test.js
@@ -1,7 +1,8 @@
 const { test, describe, expect } = require('@jest/globals');
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../index')
+const app = require('../index');
+const Blog = require('../models/blog');
 const api = supertest(app)
 
 describe('GET /api/blogs', () => {
@@ -10,6 +11,51 @@ describe('GET /api/blogs', () => {
             .expect(200)
             .expect('Content-Type', /application\/json/)
 
-        expect(response.body).toHaveLength(2)
+        expect(response.body).toHaveLength(7)
     })
+
+    test('blogs have an id field', async () => {
+        const response = await api.get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        response.body.forEach(blog => {
+            expect(blog.id).toBeDefined();
+        })
+    })
+
+
+
+test('blogs can be added via HTTP POST request', async () => {
+
+    const newBlog = {
+        title: '2 Blog',
+        author: 'Me Author',
+        url: 'http://www.example.com',
+        likes: 105
+    }
+
+    const initialBlogs = await api.get('/api/blogs')
+    const initialBlogCount = initialBlogs.body.length
+    console.log(newBlog)
+    await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const updatedBlogCount = response.body.length
+
+    expect(updatedBlogCount).toBe(initialBlogCount + 1)
+
+    const addedBlog = response.body[response.body.length - 1]
+    expect(addedBlog.author).toBe(newBlog.author)
+    expect(addedBlog.url).toBe(newBlog.url)
+    expect(addedBlog.likes).toBe(newBlog.likes)
+    })
+
 })
+
+afterAll(async () => {
+    await mongoose.connection.close()
+  })
