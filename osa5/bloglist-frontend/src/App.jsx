66ad9a24ae7c8b4767e206630c6741c700925a05
Wbeggin <weertim@gmail.com>
2024-04-12 14:42:07 +0300

    5.3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -7,7 +7,10 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')   
   const [password, setPassword] = useState('') 
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null)    
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -35,6 +38,7 @@ const App = () => {
       setUsername('')
       setPassword('')
       setUser(user)
+      blogService.setToken(user.token)
     } catch (exception) {
       console.log(exception)
       console.log('Wrong credentials')
@@ -67,16 +71,75 @@ const App = () => {
     </form>      
   )
 
+  const handleAddBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = {
+        title: newBlogTitle,
+        author: newBlogAuthor,
+        url: newBlogUrl
+      }
+      const returnedBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(returnedBlog))
+      setNewBlogTitle('')
+      setNewBlogAuthor('')
+      setNewBlogUrl('')
+    } catch (exception) {
+      console.log(exception.message)
+    }
+  }
+
+  const blogForm = () => (
+    <form onSubmit={handleAddBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={newBlogTitle}
+          name="title"
+          onChange={({ target }) => setNewBlogTitle(target.value)}
+        />
+      </div>
+
+      <div>
+        author:
+        <input
+          type="text"
+          value={newBlogAuthor}
+          name="author"
+          onChange={({ target }) => setNewBlogAuthor(target.value)}
+        />
+      </div>
+
+      <div>
+        url:
+        <input
+          type="text"
+          value={newBlogUrl}
+          name="url"
+          onChange={({ target }) => setNewBlogUrl(target.value)}
+        />
+        
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+
 
   return (
     <div>
       {!user && loginForm()}
       {user && <div>
          <p>{user.name} logged in</p>
+
         <button onClick={() => {
           window.localStorage.removeItem('loggedBlogappUser')
           setUser(null)
         }}>logout</button>
+
+        <h2>create new</h2>
+        {blogForm()}
+
          <h2>blogs</h2>
          {blogs.map(blog =>
            <Blog key={blog.id} blog={blog} />
