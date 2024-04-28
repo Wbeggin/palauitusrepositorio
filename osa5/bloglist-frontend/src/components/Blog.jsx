4423801f96263e0d6320c9b6286b938e53796dfa
Wbeggin <weertim@gmail.com>
2024-04-28 15:38:53 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleToggleInfo = () => {
+    setShowInfo(!showInfo)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={handleToggleInfo}>
+        {showInfo ? 'hide' : 'view'}
+      </button>
+      {showInfo && (
+        <div>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes}</p> 
+          <p>Username: {blog.user.username}</p>            
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
